5ed0b37ab7c0d3d9760bdc3b1577025459e89c2c
JuzChoco <2100812@sit.singaporetech.edu.sg>
2023-09-26 16:09:51 +0800

    Leroy

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,65 @@
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    //[Route("api/[controller]")] // Can also do this and it will use VillaAPI but not preferred as might have drawbacks in the future
+    [Route("api/VillaPI")]
+    [ApiController ]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
+        {
+               return Ok(VillaStore.GetAllVillaDTOs);
+
+        }
+
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        //[ProducesResponsesType(200, Type = typeof(VillaDTO))] //Can do this too if you just put public ActionResult GetVilla(int id); just more tedious
+        public ActionResult<VillaDTO> GetVilla(int id)
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+
+            var villa = VillaStore.GetAllVillaDTOs.FirstOrDefault(u => u.Id == id);
+            
+            if (villa == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(villa);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<VillaDTO> CreateVilla([FromBody] VillaDTO villa)
+        {
+            if (villa == null)
+            {
+                return BadRequest(villa);
+            }
+
+            if (villa.Id > 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            villa.Id = VillaStore.GetAllVillaDTOs.OrderByDescending(u  => u.Id).FirstOrDefault().Id + 1;
+            VillaStore.GetAllVillaDTOs.Add(villa);
+
+            return Ok(villa);
+        }
+    }
+}
---
+++ MagicVilla_VillaAPI/Data/VillaStore.cs
@@ -0,0 +1,14 @@
+﻿using MagicVilla_VillaAPI.Models.Dto;
+
+namespace MagicVilla_VillaAPI.Data
+{
+    public static class VillaStore
+    {
+        //Temporary Data Storage so that the Controller class can call from this file instead of hard coding all the villas in that file
+        public static List<VillaDTO> GetAllVillaDTOs = new List<VillaDTO>
+        {
+                new VillaDTO{Id = 1, Name = "Pool View"},
+                new VillaDTO{Id = 2, Name = "Beach View"}
+        };
+    }
+}
